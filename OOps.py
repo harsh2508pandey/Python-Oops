d87dadef9f6665c8cdcd889294773c4b52f37e7e
Harsh Pandey <119357977+harsh2508pandey@users.noreply.github.com>
2022-12-28 16:06:42 +0530

    Add files via upload

---
+++ OOps.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+class student():
+    name='Rahul'
+    Prog='CSE'
+    fee='114000'
+    
+
+
+# In[5]:
+
+
+a=student()
+print(a.name)
+print(a.fee)
+print(a.Prog)
+
+
+# In[7]:
+
+
+def per(c1,c2,c3):
+    Totalmarks=c1+c2+c3
+    percentage=Totalmarks/3
+    return percentage
+
+
+# In[ ]:
+
+
+
+
